02ca15f88e599638670952179d44c94d65af2e69
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-10-23 12:45:47 -0300

    23102023:12:45

---
+++ type-numbers.js
@@ -0,0 +1,23 @@
+//tipo number
+const meunumero=3;
+
+
+const primeironumero=1;
+const segundonumero=2;
+
+const operacaomatematica=primeironumero - segundonumero;
+
+
+
+console.log(operacaomatematica)
+
+//ponto flutuante numeres reais
+const numeropontoflutuante=3.3;
+const pontoflutuantesemzero=.5;
+const novaOperacao=primeironumero/numeropontoflutuante;
+
+console.log(novaOperacao);
+
+// NaN -> Not A NUMBer (não é numero)
+const alura ="Alura";
+console.log
