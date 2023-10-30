f9cfdd885019a39225e3140fda60c3ee3618fb6c
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-10-30 14:43:35 -0300

    atualizado 30:10:2023:14:43

---
+++ Para-saber-mais .js
@@ -0,0 +1,39 @@
+/*
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone)); 
+// teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+*/
+/*
+let telefone = 12341234;
+console.log("O telefone é " + telefone.toString()); // o .toString() é uma outra forma para  fazer essa conversão, que é mais parecida com outras linguagens de programação.
+*/
+/*
+let usuarioConectado = false;
+console.log(String(usuarioConectado)); // teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+usuarioConectado = true;
+console.log(String(usuarioConectado)); // agora teremos uma string “true”.*/
+
+// Vamos calcular a área de um retângulo
+/*let largura = "10";
+let altura = "5";
+console.log(Number(largura) * Number(altura)); // teremos a conversão de String para números, possibilitando a realização da multiplicação
+*/
+
+/*
+let largura = "10";
+let altura = "5";
+console.log( + largura * + altura); */
+// teremos a conversão de String para números realizado usando o + antes das variáveis
+//da NaN mesmo faz parte do código passado pelo Alura
+//let meuNome = "leonardo";
+//console.log(Number(meuNome)); // como a variável meuNome não contém apenas números ele retorna o erro NaN (Not a Number, não é número);
+//console.log( + meuNome); // a conversão também retornará NaN
+
+//let cpfUsuario = "12312312312";
+
+//let var = 0;
+//let if = 0;
+//let const = "Alura";
+let varInicial = 0;
+let ifFalse = 0;
+let constDeTexto = "Alura";
