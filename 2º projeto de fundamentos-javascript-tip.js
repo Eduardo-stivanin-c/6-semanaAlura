193eb656294255d52905029962dbd727b184fe7c
Eduardo-stivanin-c <1sfr2d@gmail.com>
2023-10-30 11:38:10 -0300

    30102023:11:38 atradsado

---
+++ 2º projeto de fundamentos-javascript-tip.js
@@ -0,0 +1,26 @@
+//2º projeto de fundamentos-javascript-tipos-variaveis-funcoes
+//var altura=5;
+//var comprimento=7;
+//area =altura * comprimento;
+//console.log(area);
+
+//var  area;
+/*let forma='retângulo';
+let altura=5;
+let comprimento=7;
+//let só funciona depois de declarada
+//let loca l com controle e var muito solto
+let area;
+
+//se forma=retângulo
+if (forma==="retângulo") {
+    area =altura *comprimento;
+
+    
+} else {
+    //senão calcula área  do triângulo 
+    area =altura *comprimento/2;
+
+}
+console.log(area);*/
+
--- fundamentos-javascript-tipos-variaveis-f.js
+++ fundamentos-javascript-tipos-variaveis-f.js
@@ -44,3 +44,40 @@ console.log(cidade === inputMinusculo); // true
 
 const senha = "minhaSenha123"
 console.log(senha.length) // 13 caracteres
+
+
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
+
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+let input = null;
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
+
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10
+
+
+
+
+
+//fundamentos-javascript-tipos-variaveis-funcoes/task/94131 
+//
